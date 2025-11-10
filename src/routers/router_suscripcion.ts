1243edb83d9a625176145de854d0be1750a3660d
luisbaquiax <luis.baquiax1995@gmail.com>
2025-11-10 04:41:33 -0600

    cancelar suscripcion

--- src/routers/router_suscripcion.ts
+++ src/routers/router_suscripcion.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { suscribirsePeriodista, obtenerSuscripcionesUsuario } from "../controllers/suscripcion_controller";
+import { suscribirsePeriodista, obtenerSuscripcionesUsuario, cancelarSuscripcion } from "../controllers/suscripcion_controller";
 
 const suscripcionRouter = Router();
 
 suscripcionRouter.post("/suscribirse", suscribirsePeriodista);
 suscripcionRouter.get("/suscripciones/:id_usuario", obtenerSuscripcionesUsuario);
+suscripcionRouter.delete("/cancelar", cancelarSuscripcion);
 
 export default suscripcionRouter;  
